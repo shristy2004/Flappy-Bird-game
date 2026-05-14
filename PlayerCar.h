7b34be9b5f4a23a26fda6db8b9ecc2acbec2e589
Shristy <shristy712@gmail.com>
2026-05-14 23:46:34 +0530

    Update PlayerCar.h

--- PlayerCar.h
+++ PlayerCar.h
@@ -1,3 +1,10 @@
 #pragma once
 #include "Car.h"
 #include <vector>
+class PlayerCar : public Car {
+private:
+ std::vector<float> lanes;
+ int currentLane;
+ float y;
+public:
+ PlayerCar(Texture& texture) {
