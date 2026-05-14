0582fb55c88bb2ba1a6d33f278948b0264d7eddd
Shristy <shristy712@gmail.com>
2026-05-14 23:47:07 +0530

    Update PlayerCar.h

--- PlayerCar.h
+++ PlayerCar.h
@@ -8,3 +8,26 @@ private:
  float y;
 public:
  PlayerCar(Texture& texture) {
+sprite.setTexture(texture);
+ sprite.setScale(0.35f, 0.35f);
+ FloatRect bounds = sprite.getLocalBounds();
+ sprite.setOrigin(bounds.width / 2, bounds.height / 2);
+ lanes = {150, 400, 650};
+ currentLane = 1;
+ y = 500;
+ sprite.setPosition(lanes[currentLane], y);
+ }
+ void moveLeft() {
+ if (currentLane > 0) {
+ currentLane--;
+ sprite.setPosition(lanes[currentLane], y);
+ }
+ }
+ void moveRight() {
+ if (currentLane < 2) {
+ currentLane++;
+ sprite.setPosition(lanes[currentLane], y);
+ }
+ }
+ void move(float dt) override {
+ 
