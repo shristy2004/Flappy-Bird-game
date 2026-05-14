e81c8f5804547451e2e5c10a81f1930b7b871308
Shristy <shristy712@gmail.com>
2026-05-15 00:12:31 +0530

    Set sprite origin based on local bounds

--- EnemyFile.h
+++ EnemyFile.h
@@ -8,3 +8,6 @@ public:
  EnemyCar(Texture& texture, float x, int lane) {
  sprite.setTexture(texture);
  sprite.setScale(0.35f, 0.35f);
+FloatRect bounds = sprite.getLocalBounds();
+ sprite.setOrigin(bounds.width / 2, bounds.height / 2);
+
