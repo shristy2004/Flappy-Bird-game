652ca3c56315b70ed0242db16c3d54b6c5d95360
Shristy <shristy712@gmail.com>
2026-05-14 23:44:53 +0530

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+class Car {
+protected:
+ Sprite sprite;
+public:
+ virtual void move(float dt) = 0;
+ Sprite& getSprite() {
+ return sprite;
+ }
+};
 
