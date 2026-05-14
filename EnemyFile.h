667b31a77abda48a14552156f3bdec77617b2ee6
Shristy <shristy712@gmail.com>
2026-05-15 00:12:14 +0530

    Update EnemyFile.h

--- EnemyFile.h
+++ EnemyFile.h
@@ -4,3 +4,7 @@ class EnemyCar : public Car {
 private:
  float speed;
  int laneIndex;
+public:
+ EnemyCar(Texture& texture, float x, int lane) {
+ sprite.setTexture(texture);
+ sprite.setScale(0.35f, 0.35f);
