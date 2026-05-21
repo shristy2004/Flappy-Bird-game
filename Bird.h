445f670f442a9893fdf850c9d500b1e0c19fad1a
Shristy <shristy712@gmail.com>
2026-05-21 21:32:10 +0530

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -8,4 +8,9 @@ float gravity;
  float flapStrength;
 static const float RENDER_W;
  static const float RENDER_H;
+Bird(sf::Texture& texture, float startX, float startY) {
+ velocityY = 0.f;
+ gravity = 1000.f;
+ flapStrength = -380.f;
+ sprite.setTexture(texture);
 
